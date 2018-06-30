606ab32e80331db2036fa63b8e3059b4584e6a46
XIEMI2 <mike.xie@oocl.com>
2018-06-30 17:58:42 +0800

    introduce two plugins: html-webpack-plugin, clean-webpack-plugin

--- demo-04-manage-output/readme.md
+++ demo-04-manage-output/readme.md
@@ -1,2 +1,10 @@
 # Introduce
-Multiple entrance case
+1. Multiple entrance case
+
+# Plugins
+1. Introduce HtmlWebpackPlugin
+
+2. CleanWebpackPlugin
+
+Click here to get more information
+https://www.webpackjs.com/guides/output-management/
--- demo-04-manage-output/webpack.config.js
+++ demo-04-manage-output/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
     entry: {
@@ -9,13 +11,19 @@ module.exports = {
         filename: '[name].bundle.js', /*Dynamic generate the bundle name*/
         path: path.resolve(__dirname, 'dist')
     },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),/*每次构建前清理 /dist 文件夹, 因此只会生成用到的文件*/
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     module: {
-        rules:[
+        rules: [
             {
                 test: /\.css$/,
                 use: [
-                    { loader: "style-loader"},
-                    { loader: "css-loader"}
+                    {loader: "style-loader"},
+                    {loader: "css-loader"}
                 ]
             },
             {
