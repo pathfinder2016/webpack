0eab9950b822960187827dcfcaeee4d4930ed5f8
XIEMI2 <mike.xie@oocl.com>
2018-06-30 12:03:39 +0800

    introduce webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import _ from 'lodash'
+/*显示要求引入 lodash*/
+/*通过声明模块所需的依赖，webpack 能够利用这些信息去构建依赖图，然后使用图生成一个优化过的，会以正确顺序执行的 bundle。*/
 function component() {
     var element = document.createElement('div');
 
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
